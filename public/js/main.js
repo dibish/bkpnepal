b4374edfc590c6bb86a9e105ea9b08ea17f357d0
Dipin Bishwakarma <dibish.info@gmail.com>
2022-03-25 18:29:07 +0545

    upcoming page added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+//current page link highliting
+const splitedPath = window.location.pathname.split('/')
+const pageName = splitedPath[splitedPath.length - 1]
+const navLinks = document.querySelectorAll('.nav-links')
+
+navLinks.forEach(element => {
+    const linkParts = element.href.split('/')
+    const currentPage = linkParts[linkParts.length - 1]
+    console.log(currentPage)
+    if(pageName === currentPage){
+        element.classList.add('underline', 'decoration-brand-600','decoration-4','underline-offset-8')
+    }
+});
+//end of current page link highliting
+
+
+
 //Side nav toggle funcationality
 const menu = document.querySelector('.menu')
 const mobileSideNav = document.querySelector('.mobile-side-nav')
